6adf4a14e01c3ab751fdd2e6d9368d066961f541
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-13 10:25:04 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Importação dos módulos
 import { searchAcademies } from "./modules/searchAcademies.js";
 import { ClearOptions } from "./modules/ClearOptions.js";
+import { searchForGyms } from "./modules/Gyms.js";
 
 // Selecionando os botões
 const ButtonSearch = document.querySelector(".find");
@@ -8,5 +9,6 @@ const ClearButton = document.querySelector(".clean");
 
 // Adicionando eventos de clique aos botões
 document.addEventListener("DOMContentLoaded", searchAcademies);
+document.addEventListener("DOMContentLoaded", searchForGyms);
 ButtonSearch.addEventListener("click", searchAcademies);
 ClearButton.addEventListener("click", ClearOptions);
