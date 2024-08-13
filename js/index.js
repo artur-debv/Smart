e71f8d6b1b520a60dc6bc609c97680db7e97db7a
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-13 11:43:08 -0300

    feat: I added the functionality to return to the top of the page when clicking the back button

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@
 import { searchAcademies } from "./modules/searchAcademies.js";
 import { ClearOptions } from "./modules/ClearOptions.js";
 import { searchForGyms } from "./modules/Gyms.js";
+import { BackToTop } from "./modules/BackToTop.js";
 
 // Selecionando os botões
 const ButtonSearch = document.querySelector(".find");
 const ClearButton = document.querySelector(".clean");
 
+
 // Adicionando eventos de clique aos botões
+document.addEventListener("scroll", BackToTop);
 document.addEventListener("DOMContentLoaded", searchAcademies);
 ButtonSearch.addEventListener("click", searchForGyms);
 ClearButton.addEventListener("click", ClearOptions);
