647c51c514776ba1e4c9d6851ce7c03c3a74ef3f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-13 11:08:45 -0300

    updates

--- js/modules/searchAcademies.js
+++ js/modules/searchAcademies.js
@@ -3,9 +3,9 @@ import { displayCount } from "./count.js";
 import { academiesCards } from "./academiesCards.js";
 import { filterAcademy } from "./filterAcademy.js";
 
- export const searchAcademies = async (event) => {
+ export const searchAcademies = async () => {
    
-    event.preventDefault(); 
+   
 
     const academies = await fetchApi();
     const academiesFilter = filterAcademy(academies);
